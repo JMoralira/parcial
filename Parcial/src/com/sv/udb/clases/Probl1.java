b4c21334cb0e89f73af2c842b9b016a9558ed123
Jose Lira <Jose Lira@Jose-PC>
2017-02-18 11:57:52 -0600

    Parcial

---
+++ Parcial/src/com/sv/udb/clases/Probl1.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.clases;
+
+import java.awt.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author Jose Lira
+ */
+public class Probl1 {
+    private String Nombre;
+    private int Edad;
+    private double Altura;
+    private double Peso;
+
+   /*  public Probl1(String Nombre , int Edad, double Altura, double Peso) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+        this.Altura = Altura;
+        this.Altura = Peso;
+    }
+    
+    public Probl1()
+    {}*/
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Edad
+     */
+    public int getEdad() {
+        return Edad;
+    }
+
+    /**
+     * @param Edad the Edad to set
+     */
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    /**
+     * @return the Altura
+     */
+    public double getAltura() {
+        return Altura;
+    }
+
+    /**
+     * @param Altura the Altura to set
+     */
+    public void setAltura(double Altura) {
+        this.Altura = Altura;
+    }
+
+    /**
+     * @return the Peso
+     */
+    public double getPeso() {
+        return Peso;
+    }
+
+    /**
+     * @param Peso the Peso to set
+     */
+    public void setPeso(double Peso) {
+        this.Peso = Peso;
+    }
+    
+    
+}
+
---
+++ Parcial/src/parcial/Parcial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+
+import com.sv.udb.forms.frmMenu;
+
+/**
+ *
+ * @author Jose Lira
+ */
+public class Parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmMenu obje = new frmMenu();
+        obje.setVisible(true);
+    }
+    
+}
